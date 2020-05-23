e828e829fab8222b8f87b8296c4d7d7e1461a9cb
junliliu <576471639@qq.com>
2020-05-23 15:59:46 +0800

    by 2020.5.23

--- 八皇后问题-递归解法.py
+++ 八皇后问题-递归解法.py
@@ -3,7 +3,7 @@ def conflict(state, next_y):
     state:棋子的状态
     next_y:检测的棋子所在行数
     """
-    next_x = len(state)  # 遍历state中的元素
+    next_x = len(state)  # x的坐标
     for i in range(next_x):
         if abs(state[i] - next_y) in (0, next_x - i):
             # 在检测皇后与前面的皇后 y的差值为0时：水平（在同一行）；y的差值 = x的差值时候（为斜边）
@@ -29,6 +29,7 @@ def queens(num=8, state=()):
                     yield result + (pos,)
 
 
-list(queens(4))
+list(queens(8))
+
 
 
